0efcba348630ce72e5be42e777b8916e049ca5ab
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2022-02-21 20:00:53 -0800

    Understand type annotation vs inference, and function annotations

---
+++ typescript-the-complete-developers-guide/features/functions.ts
@@ -0,0 +1,30 @@
+const add = (a: number, b: number): number => {
+    return a + b;
+};
+
+const sub = (a: number, b: number): number => {
+    return a - b;
+};
+
+function div(a: number, b: number): number {
+    return a / b;
+}
+
+const mult = function(a: number, b: number): number {
+    return a * b;
+}
+
+console.log("1 + 9 =", add(1, 9));
+console.log("1 - 9 =", sub(1, 9));
+console.log("9 / 3 =", div(9, 3));
+console.log("4 * 3 =", mult(3, 4));
+
+const logger = (msg: string): void => {
+    console.log(msg);
+    // you can return undefined
+    return undefined;
+};
+
+// const throwErr = (msg: string): never => {
+//     throw new Error(msg);
+// };
--- typescript-the-complete-developers-guide/features/variables.ts
+++ typescript-the-complete-developers-guide/features/variables.ts
@@ -1,3 +1,4 @@
+// if you declare the variable and initialize it on the same line, Typescript will infer the type.
 let apples: number = 5;
 let speed: string = "fast";
 let found: boolean = true;
@@ -30,3 +31,42 @@ let point: {x: number; y: number} = {
 const logNumber: (i: number) => void = (i: number) => {
     console.log(i);
 };
+
+
+/* freestyling */
+let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const sum = numbers.reduce((prev, curr) => {
+    return prev + curr
+}, 0);
+
+console.log("Average =", sum / 2);
+
+// When to use annotations
+// 1) Function returns the 'any' type
+
+const json = '{"x": 10, "y": 20}';
+const coordinates: {x: number, y: number} = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] == "green") {
+        foundWord = true;
+        break;
+    }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+numbers = [-1, 20, 0];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
+}
