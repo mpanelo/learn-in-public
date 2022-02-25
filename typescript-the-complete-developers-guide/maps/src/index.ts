6a6f78c58a66b9c6ef81380a952899d54f82809a
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2022-02-24 20:29:14 -0800

    Add skeleton code for maps project

---
+++ typescript-the-complete-developers-guide/maps/src/index.ts
@@ -0,0 +1,18 @@
+/// <reference types="@types/google.maps" />
+
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+const company = new Company();
+
+console.log(user);
+console.log(company);
+
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
