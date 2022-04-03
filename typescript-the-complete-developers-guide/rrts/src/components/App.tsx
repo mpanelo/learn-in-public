87717a6a97e71e59d2c0d2998317433241a65caf
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2022-04-02 22:32:03 -0700

    Support todo deletion

--- typescript-the-complete-developers-guide/rrts/src/components/App.tsx
+++ typescript-the-complete-developers-guide/rrts/src/components/App.tsx
@@ -1,21 +1,48 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Todo, fetchTodos } from "../actions";
+import { Todo, fetchTodos, deleteTodo } from "../actions";
 import { StoreState } from "../reducers";
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
 
-class _App extends React.Component<AppProps> {
+interface AppState {
+  loading: boolean;
+}
+
+class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { loading: false };
+  }
+
   onButtonClick = (): void => {
     this.props.fetchTodos();
+    this.setState({ loading: true });
+  };
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ loading: false });
+    }
+    console.log(this.state);
+  }
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
   };
 
   renderList(): JSX.Element[] {
     return this.props.todos.map((todo: Todo) => {
-      return <div key={todo.id}>{todo.title}</div>;
+      return (
+        <div key={todo.id} onClick={() => this.onTodoClick(todo.id)}>
+          {todo.title}
+        </div>
+      );
     });
   }
 
@@ -23,6 +50,7 @@ class _App extends React.Component<AppProps> {
     return (
       <div>
         <button onClick={this.onButtonClick}>Fetch Todos</button>
+        {this.state.loading ? "LOADING" : null}
         {this.renderList()}
       </div>
     );
@@ -33,4 +61,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   return { todos };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
